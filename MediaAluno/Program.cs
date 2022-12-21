bc8efab57761b9011380881c94a00707b2b862ff
JArnaldo88 <99845582+JArnaldo88@users.noreply.github.com>
2022-12-20 21:42:27 -0300

    Add files via upload

---
+++ MediaAluno/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace mediaAluno
+{
+    internal  class program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, media;
+            Console.WriteLine("insira a primeira nota:");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("insira a segunda nota:");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("insira a terceira nota:");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            media = Math.Round (n1 + n2 + n3) / 3;
+
+            if (media <= 5)
+            {
+                Console.WriteLine("Reprovado"  + media);
+            }
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado" + media);
+            }
+            if(media > 5 && media < 7)
+            {
+                Console.WriteLine("Recuperação" + media );
+            }
+        }
+    }
+}
+
